e2c7950e8e55975b7148e7cf7e33f192c571e915
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-10-08 12:40:18 +0300

    add assets folder and kPrimaryColor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(const BooklyApp() );
+  runApp(const BooklyApp());
 }
 
 class BooklyApp extends StatelessWidget {
@@ -12,6 +13,10 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
   }
