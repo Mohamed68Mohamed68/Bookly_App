264e26fa69c47be9b67a9141c2ab701a8f0643bc
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-10-12 09:39:36 +0300

    best seller item part 3

---
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,70 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.TestImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
