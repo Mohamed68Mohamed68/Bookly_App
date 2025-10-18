8fda4713e32509399a02b7de99b47f3324296f37
Mohamed68Mohamed68 <demo963210@gmail.com>
2025-10-18 03:28:43 +0300

    create custom book details app bar

--- lib/Features/home/presentation/views/book_details_view.dart
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -6,6 +6,10 @@ class BookDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BookDetailsViewBody();
+    return Scaffold(
+      body: SafeArea(
+        child: BookDetailsViewBody(),
+      ),
+    );
   }
 }
